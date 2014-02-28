b705c2703f7a9e36afba21e0c0a3c5261a6fffdb
Convex Function <329364@gehry.mathepool.rwth-aachen.de>
2014-02-28 14:53:42 +0100

    Compile against OpenMPI.

--- src/dune_ug_hpc.cc
+++ src/dune_ug_hpc.cc
@@ -8,7 +8,7 @@
 #include <dune/grid/uggrid.hh>
 #include <dune/grid/utility/structuredgridfactory.hh>
 
-//#include <dune/common/parallel/mpihelper.hh> // An initializer of MPI
+#include <dune/common/parallel/mpihelper.hh> // An initializer of MPI
 #include <dune/common/exceptions.hh>
 #include <dune/common/parametertree.hh>
 #include <dune/common/parametertreeparser.hh>
@@ -24,6 +24,9 @@ std::string toString(const T& t) {
   return static_cast<std::ostringstream*>( &(std::ostringstream() << t) )->str();
 }
 
+// Helper macro to execute code just on rank zero process
+#define RANK0_OPERATION(CODE) do { if (0 == mpihelper.rank()) { CODE } } while(0)
+
 // Define some units, constants and types
 const int dim = 3;
 
@@ -38,6 +41,11 @@ typedef GV::Codim<0>::Iterator ElementIterator;
 
 int main(int argc, char** argv) try
 {
+  // Create MPIHelper instance
+  Dune::MPIHelper& mpihelper = Dune::MPIHelper::instance(argc, argv);
+
+  RANK0_OPERATION(std::cout << "Using " << mpihelper.size() << " Processes." << std::endl;);
+
   // Parse parameter file
   const std::string parameterFileName = "param.ini";
 
@@ -69,10 +77,10 @@ int main(int argc, char** argv) try
   const int levels = parameterSet.get<int>("levels");
 
   for (size_t s = 0; s < steps; ++s) {
-    std::cout << "Step " << s << " ..." << std::endl;
+    RANK0_OPERATION(std::cout << "Step " << s << " ..." << std::endl;);
 
     for (int k = 0; k < levels; ++k) {
-      std::cout << "   Refining level " << k << " ..." << std::endl;
+      RANK0_OPERATION(std::cout << "   Refining level " << k << " ..." << std::endl;);
 
       // select elements that are close to the sphere for grid refinement
       for (ElementIterator eIt = gv.begin<0>(); eIt != gv.end<0>(); ++eIt) {
@@ -88,10 +96,16 @@ int main(int argc, char** argv) try
     }
 
     // Output grid
+    mpihelper.getCollectiveCommunication().barrier();
+
+    RANK0_OPERATION(
     const std::string baseOutName = "RefinedGrid_";
     VTKWriter<GV> vtkWriter(gv);
 
     vtkWriter.write(baseOutName+toString(s));
+    );
+
+    mpihelper.getCollectiveCommunication().barrier();
 
     // If this is not the last step, move sphere and coarsen grid
     if (s+1 < steps) {

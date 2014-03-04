38d4a969f9760766020b9bc828c7209af7ce3fa3
Convex Function <329364@gehry.mathepool.rwth-aachen.de>
2014-03-04 22:26:47 +0100

    Program crashes due to undiscerning usage of DUNE MPI functionality.

--- src/dune_ug_hpc.cc
+++ src/dune_ug_hpc.cc
@@ -24,33 +24,31 @@ std::string toString(const T& t) {
   return static_cast<std::ostringstream*>( &(std::ostringstream() << t) )->str();
 }
 
-// Helper macro to execute code just on rank zero process
-#define RANK0_OPERATION(CODE) do { if (0 == mpihelper.rank()) { CODE } } while(0)
-
 // Define some units, constants and types
 const int dim = 3;
-
-const double mm = 1e-3;
 
 typedef FieldVector<double, dim> GlobalVector;
 
 typedef UGGrid<dim> GridType;
 typedef GridType::LeafGridView GV;
-typedef GV::Codim<0>::Iterator ElementIterator;
+
+typedef GV::Codim<0>::Partition<Interior_Partition>::Iterator ElementIterator;
 
 
 int main(int argc, char** argv) try
 {
   // Create MPIHelper instance
-  Dune::MPIHelper& mpihelper = Dune::MPIHelper::instance(argc, argv);
+  MPIHelper& mpihelper = MPIHelper::instance(argc, argv);
 
-  RANK0_OPERATION(std::cout << "Using " << mpihelper.size() << " Processes." << std::endl;);
+  if (0 == mpihelper.rank()) {
+    std::cout << "Using " << mpihelper.size() << " Processes." << std::endl;
+  }
 
   // Parse parameter file
   const std::string parameterFileName = "param.ini";
 
-  Dune::ParameterTree parameterSet;
-  Dune::ParameterTreeParser::readINITree(parameterFileName, parameterSet);
+  ParameterTree parameterSet;
+  ParameterTreeParser::readINITree(parameterFileName, parameterSet);
 
   // Create ug grid from structured grid
   const std::array<unsigned, dim> n = parameterSet.get<std::array<unsigned, dim> >("n");
@@ -60,8 +58,10 @@ int main(int argc, char** argv) try
     upper = parameterSet.get<GlobalVector>("upper");
 
   shared_ptr<GridType> grid = StructuredGridFactory<GridType>::createSimplexGrid(lower, upper, n);
-  const GV gv = grid->leafView();
 
+  grid->loadBalance();
+
+  const GV gv = grid->leafView();
 
   // Create ball
   const GlobalVector center = parameterSet.get<GlobalVector>("center");
@@ -76,14 +76,15 @@ int main(int argc, char** argv) try
   const double epsilon = parameterSet.get<double>("epsilon");
   const int levels = parameterSet.get<int>("levels");
 
+
   for (size_t s = 0; s < steps; ++s) {
-    RANK0_OPERATION(std::cout << "Step " << s << " ..." << std::endl;);
+    std::cout << "Step " << s << " on " << mpihelper.rank() << " ..." << std::endl;
 
     for (int k = 0; k < levels; ++k) {
-      RANK0_OPERATION(std::cout << "   Refining level " << k << " ..." << std::endl;);
+      std::cout << "   Refining level " << k << " on " << mpihelper.rank() << " ..." << std::endl;
 
       // select elements that are close to the sphere for grid refinement
-      for (ElementIterator eIt = gv.begin<0>(); eIt != gv.end<0>(); ++eIt) {
+      for (ElementIterator eIt = gv.begin<0, Interior_Partition>(); eIt != gv.end<0, Interior_Partition>(); ++eIt) {
 	if (ball.distanceTo(eIt->geometry().center()) < epsilon)
 	  grid->mark(1, *eIt);
       }
@@ -96,16 +97,10 @@ int main(int argc, char** argv) try
     }
 
     // Output grid
-    mpihelper.getCollectiveCommunication().barrier();
-
-    RANK0_OPERATION(
     const std::string baseOutName = "RefinedGrid_";
+
     VTKWriter<GV> vtkWriter(gv);
-
     vtkWriter.write(baseOutName+toString(s));
-    );
-
-    mpihelper.getCollectiveCommunication().barrier();
 
     // If this is not the last step, move sphere and coarsen grid
     if (s+1 < steps) {
@@ -113,7 +108,7 @@ int main(int argc, char** argv) try
       ball.center += stepDisplacement;
 
       // Coarsen everything
-      for (ElementIterator eIt = gv.begin<0>(); eIt != gv.end<0>(); ++eIt)
+      for (ElementIterator eIt = gv.begin<0, Interior_Partition>(); eIt != gv.end<0, Interior_Partition>(); ++eIt)
 	grid->mark(-levels, *eIt);
 
       // adapt grid
